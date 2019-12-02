5dafa4455ec6b0a9211e7ff6430f3b9064fb4941
pokuwagata <pokuwagata@gmail.com>
2019-12-02 15:09:53 +0000

    [Infra] fix for permission error

--- start-ec2.sh
+++ start-ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # for EC2 instance
-sudo chown 999:999 mysql/logs/error.log && docker-compose -f docker-compose.prod.yml up --build
+sudo chown 999:999 mysql/logs/error.log && sudo docker-compose -f docker-compose.prod.yml up --build
