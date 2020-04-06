14d5c81e5c788392fcfe2d1c01b8d39e7a79b73b
pokuwagata <pokuwagata@gmail.com>
2020-04-06 22:53:17 +0900

    [BFF] logging, dev/prod mode

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -5,20 +5,39 @@ import resolvers from "./resolvers";
 import expressPlayground from "graphql-playground-middleware-express";
 import "graphql-import-node";
 import typeDefs from "./typeDefs.graphql";
+import { createStream } from "rotating-file-stream";
+import path from "path";
+import morgan from "morgan";
+const { ApolloLogExtension } = require("apollo-log");
 
 var app = express();
+
+const dev = app.get("env") === "DEV";
+const extensions = [() => new ApolloLogExtension()];
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  debug: true
+  extensions,
+  debug: dev,
 });
 server.applyMiddleware({ app });
 
-app.get(`/`, (req: Request, res: Response) => res.end(`hellooooooooo`));
-app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }));
+const accessLogStream = createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "logs"),
+});
+app.use(morgan("combined", { stream: accessLogStream }));
+
+if (dev) {
+  app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }));
+}
+
+app.get(`/health-check`, (req: Request, res: Response) => res.end(`It works!`));
 
 app.listen({ port: 4000 }, () =>
-  // console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-  console.log("port 4000")
+  console.log(
+    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}\n` +
+    `mode: ${dev ? 'dev' : 'prod'}`
+  )
 );
